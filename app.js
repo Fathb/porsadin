3c8188badd5de4e61988f6c6df0e059fe4aaa540
Fathb <45653036+Fathb@users.noreply.github.com>
2021-07-16 20:43:08 +0700

    Update app.js
    add spreadsheets

--- app.js
+++ app.js
@@ -1,21 +1,9 @@
-const nik = document.querySelector("#nik");
-const nama = document.querySelector("#namaLengkap");
-const tempatlahir = document.querySelector("#tempatlahir");
-const tglLahir = document.querySelector("#tglLahir");
-const stat = document.querySelector("#stat");
-const alamat = document.querySelector("#alamat");
-const diklat = document.querySelector("#diklat");
-const keanggotaan = document.querySelector("#keanggotaan");
-const kontak = document.querySelector("#kontak");
-const jabatan = document.querySelector("#jabatan");
-const btnSubmit = document.querySelector("button");
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxdiaP6QMkek4HYaCRlai_0uKtAql3LnHUqTDiwlPZwdNhQEWypt2Hz8JOkW6eq1qsR/exec';
+const form = document.forms['data-anggota'];
 
-btnSubmit.addEventListener("click", (e)=> {
- e.preventDefault();
- const el = e.target.offsetParent;
- console.log(el);
- //if (nik.value.length < 16) {
- //alert("nomor nik harus 16 angka");
- //return
- //}
-});
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => alert("terima kasih telah meluangkan waktu mengisi form ini"))
+      .catch(error => console.error('Error!', error.message))
+  })
