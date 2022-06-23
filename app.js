b51cc107334e1643238bf0af9885180a5558eb44
fathb <eglass.boy@gmail.com>
2022-06-23 22:45:49 +0700

    less validation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbyZhtmP2IUe5V9Y9VEWC3v4zvGTFvDJO7LibRDrAK9bWXaHz6jsGgtnhoqpK8iqRfkHAw/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxZJUzH5FKeAhysK_PqsoAnYexewv-bROXktTrmPSW8_4K1fJyL2uN9HqAhXuZVhkRF/exec';
 const form = document.forms['data-peserta'];
 const nama = document.querySelector("#nama");
 const tempatlahir = document.querySelector("#tpLhr");
@@ -10,7 +10,6 @@ const cabang = document.querySelector("#cabang");
 
 form.addEventListener('submit', e => {
   e.preventDefault()
-  console.log(tglLhr);
   if (nama.value == "") {
     alert("nama harus di isi")
     return
@@ -39,10 +38,12 @@ form.addEventListener('submit', e => {
     alert("CABANG LOMBA HARUS DIPILIH")
     return
   }
+  // console.log(new FormData(form));
+  // console.log(form);
   fetch(scriptURL, {
     method: 'POST', body: new FormData(form)})
   .then(response => alert("terima kasih telah meluangkan waktu mengisi form ini"))
-  .catch(error => console.error('Error!', error.message))
+  .catch(error => console.error('Error!', error.message));
   nama.value = "";
   tempatlahir.value = "";
   tglLahir.value = "";
